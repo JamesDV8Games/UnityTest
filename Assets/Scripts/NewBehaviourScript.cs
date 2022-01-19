60f0a02912755e57d7cf09928862b268e0948615
JamesDV8Games <j.brandon@dv8learn.org.uk>
2022-01-19 15:54:59 +0000

    Initial Commit
    Initial commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public string text="Hello and welcome to my game!";
+    [SerializeField]
+    int roundnumber=5;
+    [Range(-10,10)]
+    public float number=6f;
+    public bool IsIt;
+    public GameObject ThingIWantToFindLater;
+
+    GameObject gameobject;
+    Transform address;
+
+    public Text textfield;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (ThingIWantToFindLater!=null)
+        {
+            print("The thing that I found that I want to find later is " + ThingIWantToFindLater.name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textfield.text = "" + ThingIWantToFindLater.transform.ToString();
+    }
+}
